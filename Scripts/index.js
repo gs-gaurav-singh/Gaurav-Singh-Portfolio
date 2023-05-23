aa5907d598c7344685b25c36e59a0726f7e7c468
Gaurav <gauravsingh23699@gmail.com>
2023-05-23 22:07:14 +0530

    first commit

---
+++ Scripts/index.js
@@ -0,0 +1,18 @@
+// document.getElementById("work-experience").addEventListener("click", function () {
+//     var e = document.getElementsByClassName("modalbox");
+//     e[0].style.display = "contents";
+// });
+// document.getElementById("close").addEventListener("click", function () {
+//     var e = document.getElementsByClassName("modalbox");
+//     e[0].style.display = 'none';
+// })
+
+function overlayon() {
+    e = document.getElementsByClassName("modalbox");
+    e.style.display = "contents";
+}
+
+function overlayoff() {
+    e = document.getElementsByClassName("modal");
+    e.style.display = "none";
+}
