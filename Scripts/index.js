a769e23fc2bb42a20be14198de0007a305b18947
Gaurav <gauravsingh23699@gmail.com>
2023-05-26 00:17:36 +0530

    Fix- Automatic invoking of Modal class overlay

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,18 +1,17 @@
-// document.getElementById("work-experience").addEventListener("click", function () {
-//     var e = document.getElementsByClassName("modalbox");
-//     e[0].style.display = "contents";
-// });
-// document.getElementById("close").addEventListener("click", function () {
-//     var e = document.getElementsByClassName("modalbox");
-//     e[0].style.display = 'none';
-// })
+// To open work experince Overaly
+document.getElementById("work-experience").addEventListener("click", function () {
+    var e = document.getElementsByClassName("modalbox");
+    e[0].style.display = "contents";
+});
 
-function overlayon() {
-    e = document.getElementsByClassName("modalbox");
-    e.style.display = "contents";
-}
+// To open the education overlay
+document.getElementById("education").addEventListener("click", function () {
+    var e = document.getElementsByClassName("modalbox");
+    e[0].style.display = "contents";
+});
 
-function overlayoff() {
-    e = document.getElementsByClassName("modal");
-    e.style.display = "none";
-}
+// To close the Overlay
+document.getElementById("close").addEventListener("click", function () {
+    var e = document.getElementsByClassName("modalbox");
+    e[0].style.display = 'none';
+})
