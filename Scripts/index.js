4f3d6cc56b9222934192ae9d958c1b6c95766a68
Gaurav <gauravsingh23699@gmail.com>
2023-05-28 19:50:20 +0530

    Updated modal and it's work functioning.

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,17 +1,23 @@
 // To open work experince Overaly
 document.getElementById("work-experience").addEventListener("click", function () {
-    var e = document.getElementsByClassName("modalbox");
+    var e = document.getElementsByClassName("w-modalbox");
     e[0].style.display = "contents";
 });
 
 // To open the education overlay
 document.getElementById("education").addEventListener("click", function () {
-    var e = document.getElementsByClassName("modalbox");
+    var e = document.getElementsByClassName("e-modalbox");
     e[0].style.display = "contents";
 });
 
 // To close the Overlay
 document.getElementById("close").addEventListener("click", function () {
-    var e = document.getElementsByClassName("modalbox");
+    var e = document.getElementsByClassName("w-modalbox");
+    e[0].style.display = 'none';
+})
+
+// To close the Overlay
+document.getElementById("close1").addEventListener("click", function () {
+    var e = document.getElementsByClassName("e-modalbox");
     e[0].style.display = 'none';
 })
